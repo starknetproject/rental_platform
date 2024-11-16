6d74f0dbf05f01f3b01cb3037f5c410c6d9fc772
OWK50GA <okoriewilfrid.20201206203@futo.edu.ng>
2024-11-16 08:15:33 +0100

    first page ui begun

--- frontend/starkbnb-web/app/layout.tsx
+++ frontend/starkbnb-web/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import { StarknetProvider } from '@/app/components/starknet-provider'
+import { StarknetProvider } from '@/components/starknet-provider'
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
